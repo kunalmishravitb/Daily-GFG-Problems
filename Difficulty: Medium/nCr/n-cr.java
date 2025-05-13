adee59275c7390cbd961ef450cebc938c0c57cad
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-05-13 18:05:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int r = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.nCr(n, r));
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public int nCr(int n, int r) {
+        // code here
+        long ans = 1l;
+        for(int i=0; i<r; i++){
+            ans = ans*(n-i);
+            ans = ans/(i+1);
+        }
+        return (int) ans;
+    }
+}
