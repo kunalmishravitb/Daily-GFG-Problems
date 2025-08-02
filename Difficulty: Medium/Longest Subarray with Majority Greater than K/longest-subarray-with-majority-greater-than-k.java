d8c43dbb0179cf202780310892f55e8e0a01cbf0
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-08-02 13:32:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,72 +1,30 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
 class Solution {
-    static int longestSubarray(int[] arr, int k) {
+    public int longestSubarray(int[] arr, int k) {
         // Code Here
-        int n = arr.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int prefix[] = new int[n];
-        int ans = 0;
-        
-        for(int i=0; i<n ; i++) {
-            int prev = i>0 ? prefix[i-1] : 0;
-            
-            prefix[i] += prev;
-            prefix[i] += arr[i] > k ? 1 : -1;
-            
-            int curSum = prefix[i];
-            if(curSum>0) ans = Math.max(ans, i+1);
-            int required = curSum-1;
-            if(map.containsKey(required)) {
-                int startInd = map.get(required);
-                ans = Math.max(ans, i-startInd);
+      HashMap<Integer, Integer> map = new HashMap<>();
+         int n = arr.length;
+        int ans=0;
+        //1
+         for(int i=0; i<n; i++){
+             if(arr[i]>k)
+             arr[i]=1;
+             else 
+              arr[i]=-1;
+         }
+        int preSum=0;
+      
+        for(int i=0; i<n; i++){
+            preSum+=arr[i];
+            if(preSum>0){
+                ans=Math.max(ans, i+1);
             }
-            
-            if(!map.containsKey(prefix[i])) {
-                map.put(prefix[i], i);
-            }
-            
+             
+             if(map.containsKey(preSum-1))
+            ans=Math.max(ans, i-map.get(preSum-1));
+             
+             if(!map.containsKey(preSum))
+          map.put(preSum, i);
         }
-        
-        
         return ans;
     }
 }
-
-
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-            int n = tokens.length;
-            int[] arr = new int[n];
-
-            int i = 0;
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                arr[i] = Integer.parseInt(token);
-                i++;
-            }
-
-            int k = Integer.parseInt(br.readLine().trim());
-            System.out.println(new Solution().longestSubarray(arr, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
