f32a6c7d0fa2c103e5d53649ec8719275efe499d
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-08-18 20:17:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,56 +1,33 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_cases = Integer.parseInt(br.readLine().trim());
-
-        while (test_cases-- > 0) {
-            // Read the array from input line
-            String[] input = br.readLine().trim().split(" ");
-            int[] arr = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-            }
-
-            // Solution instance to invoke the function
-            Solution ob = new Solution();
-            int result = ob.hIndex(arr);
-
-            System.out.println(result);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
     public int hIndex(int[] citations) {
+        // code here
         int n = citations.length;
-        int maxCitation = 0;
-        for (int citation : citations) {
-            maxCitation = Math.max(maxCitation, citation);
+        int high = Integer.MIN_VALUE;
+        int low = 0;
+        
+        for(int num : citations) {
+            if(num > high) high = num;
         }
-        int[] count = new int[maxCitation + 2];
-        for (int citation : citations) {
-            count[0]++;
-            count[citation + 1]--;
-        }
-        for (int i = 1; i < count.length; i++) {
-            count[i] += count[i - 1];
-        }
-        for (int i = count.length - 1; i >= 0; i--) {
-            if (count[i] >= i) {
-                return i;
+        
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(isPossible(citations,mid,n)) {
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
-        return 0;
+        
+        return high;
+    }
+    
+    public boolean isPossible(int[] citations, int val, int n) {
+        int cnt = 0;
+        
+        for(int num : citations) {
+            if(num >= val) cnt += 1;
+        }
+        
+        return cnt >= val;
     }
 }
