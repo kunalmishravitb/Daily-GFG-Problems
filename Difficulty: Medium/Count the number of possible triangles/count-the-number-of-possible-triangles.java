1552dc0e9005e6bb169f03d2a4ab2dc74199ea3a
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-08-27 17:26:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.java
@@ -1,46 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().countTriangles(arr));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    
-    static int countTriangles(int arr[]) {
+    public int countTriangles(int arr[]) {
         // code here
+           
+        int n = arr.length;
+        if (n < 3) return 0;
+
         Arrays.sort(arr);
-        int ans=0;
-        for(int i=arr.length-1;i>=0;i--){
-            int st=0, end=i-1;
-            while(st<end){
-                if(arr[st]+arr[end]>arr[i]){
-                    ans+=end-st;
-                    end--;
-                }
-                else {
-                    st++;
+        int count = 0;
+
+        
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (arr[i] + arr[j] > arr[k]) {
+                   
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
                 }
             }
         }
-        return ans;
+
+        return count;
     }
 }
+
