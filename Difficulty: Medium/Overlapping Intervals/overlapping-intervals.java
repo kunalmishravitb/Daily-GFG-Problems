2efca7a7e8eacd2e4ba1bcc1a50508f69db49680
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-12-17 22:40:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
+++ Difficulty: Medium/Overlapping Intervals/overlapping-intervals.java
@@ -1,56 +1,39 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int[][] arr = new int[n][2];
-            for (int i = 0; i < n; i++) {
-                String temp[] = br.readLine().trim().split(" ");
-                arr[i][0] = Integer.parseInt(temp[0]);
-                String x = temp[1];
-                arr[i][1] = Integer.parseInt(x);
-            }
-            Solution obj = new Solution();
-            // The mergeOverlap function now returns a List<int[]>
-            List<int[]> ans = obj.mergeOverlap(arr);
-
-            // Printing the merged arr
-            for (int[] interval : ans) {
-                System.out.print(interval[0] + " " + interval[1] + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    public List<int[]> mergeOverlap(int[][] arr) {
-        // Code here // Code here
-        Arrays.sort(arr,(a,b)->Integer.compare(a[0],b[0]));
-        
-        List<int[]>ans=new ArrayList<>();
-        int n=arr.length;
-        
-        for(int i=0;i<n;i++){
-            int start=arr[i][0];
-            int end=arr[i][1];
-            
-            if(!ans.isEmpty() && ans.get(ans.size()-1)[1]>=start){
-                int []last=ans.get(ans.size()-1);
-                last[1]=Math.max(last[1],end);
+    public ArrayList<int[]> mergeOverlap(int[][] arr) {
+        // Code here
+        ArrayList<int[]> ansList = new ArrayList<>();
+        Arrays.sort(arr , (a , b) ->{
+            if(a[0] != b[0]){
+                return Integer.compare(a[0],b[0]);
+            }else{
+                return Integer.compare(a[1],b[1]);
             }
-            else{
-                ans.add(new int[]{start,end});
+        });
+        int n = arr.length;
+        
+        int min = arr[0][0];
+        int max = arr[0][1];
+        
+        for(int i = 1 ; i < n ; i++){
+            int val1 = arr[i][0];
+            int val2 = arr[i][1];
+            
+            if(val1 >= min && val1 <= max){
+                max = Math.max(val2 , max);
+            }else{
+                int[] newArr = new int[2];
+                newArr[0] = min;
+                newArr[1] = max;
+                ansList.add(newArr);
+                
+                min = val1;
+                max = val2;
             }
         }
-        return ans;
+        int[] newArr = new int[2];
+        newArr[0] = min;
+        newArr[1] = max;
+        ansList.add(newArr);
+        return ansList;
     }
 }
