b4edf7a0ef7d00f92e5f872f606be30bd728b8f3
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2025-12-26 20:26:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,53 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
-        int missingCount = 0;
-        int current = 1; // Start checking from 1
-        int index = 0;
-
-        while (true) {
-            // Check if the current number is missing
-            if (index < arr.length && arr[index] == current) {
-                // Number is in the array; move to the next array element
-                index++;
-            } else {
-                // Number is missing
-                missingCount++;
-                if (missingCount == k) {
-                    return current; // Found the k-th missing number
-                }
+        // code here
+        ArrayList<Integer> res=new ArrayList<>();
+        int j=0;
+        int l=arr[arr.length-1];
+        for(int i=1;res.size()<k;i++){
+            if(j<arr.length && arr[j]==i){ j++;}
+            else{
+                res.add(i);
             }
-            current++; // Move to the next number
         }
+        return res.get(k-1);
+        
     }
 }
