a3f270aea9f20eca38473ea3ba409dea6d99c557
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2026-03-30 21:01:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count the number of subarrays/count-the-number-of-subarrays.java
@@ -0,0 +1,22 @@
+class Solution 
+{ 
+    long count(int n,int arr[],long k ){
+        long ans=0;
+        long sum=0;
+        for(int l=0, r=0; r<n;r++){
+            sum+=arr[r];
+            while(sum>k){
+                sum-=arr[l];
+                l++;
+            }
+            ans+=r-l+1;
+        }
+        return ans;
+    }
+    long countSubarray(int N,int A[],long L,long R) {
+        // code here
+        long ans1=count(N,A,R);
+        long ans2=count(N,A,L-1);
+        return ans1-ans2;
+    }
+} 
