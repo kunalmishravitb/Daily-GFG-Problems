d2701a18173ba4b8fcbfc6fe70f5e044a59caa5b
Kunal Mishra <118912513+kunalmishravitb@users.noreply.github.com>
2026-06-23 20:35:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Maximum Number of People Defeated/maximum-number-of-people-defeated.java
@@ -0,0 +1,11 @@
+class Solution {
+    int maxPeopleDefeated(int p) {
+        // code here
+        int c = 0;
+        for(int i=1;p>=i*i;i++){
+            p = p-(i*i);
+            c++;
+        }
+        return c;
+    }
+};
